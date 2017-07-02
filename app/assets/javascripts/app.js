4888358b87209045170675a6cef98d0c4dd51c95
Phanousit Syhakhom <phanousitsyhakhom@Phanousit.fios-router.home>
2017-07-02 09:50:14 -0400

    first commit

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,66 @@
+$(document).ready(function(){
+
+	$('#searchInput').focus();
+
+	$('form').submit(function (evt) {
+		evt.preventDefault();
+
+  		const searchVal = $('#searchInput').val().toLowerCase();
+
+  		const rubyGems = 'https://rubygems.org/api/v1/gems/' + searchVal + '.json';  
+
+  		function getGems(data) {
+  			$.each(data, function(index,gem) {				
+  				let gemName = '<div class="panel panel-default">';
+  					gemName += '<div class="panel-heading"><h3 class="panel-title">Gem</h3></div>';
+  					gemName += '<div class="panel-body">' +
+  									'<a href="' + data.project_uri + '"' + 'target="_blank">' + data.name + '</a>' +
+  									'<span class="stars pull-right"><i class="fa fa-star-o" aria-hidden="true"></i></span>' +
+  								'</div>';
+  					gemName += '</div>';
+
+  					gemName += '<div class="panel panel-default">';
+  					gemName += '<div class="panel-heading"><h3 class="panel-title">Info</h3></div>';
+  					gemName += '<div class="panel-body">' +
+  									data.info +
+  								'</div>';
+  					gemName += '</div>';
+
+  					gemName += '<div class="panel panel-default">';
+  					gemName += '<div class="panel-heading"><h3 class="panel-title">Dependencies</h3></div>';
+  					gemName += '<div class="panel-body"><ul class="list-unstyled">' + 
+  									'<li><a href="https://rubygems.org/gems/' + 
+  											$.each(data.dependencies.runtime, function(i, dep){
+  												 dep.name
+  											}); + '" target="_blank">' + 
+  											data.dependencies.runtime[0].name + 
+  										'</a>' + 
+  										'<span class="stars pull-right">' + 
+  											'<i class="fa fa-star-o" aria-hidden="true"></i>' + 
+  										'</span>' + 
+  									'</li></ul>' + 
+  								'</div>';
+  					gemName += '</div>';
+
+  			$('#searchResults').html(gemName);
+
+
+  			$('.stars').click(function(evt){
+  				const favoritesArray = [];
+  				favoritesArray.push(data.name);
+  				console.log(favoritesArray);
+
+  				$('#favorites').html('hello world');
+  			});
+      }); // end each
+    	}
+    	$.getJSON(rubyGems, getGems).fail(function(){
+    		if(searchVal === "") {
+    			$('#searchResults').html('<h3 class="text-center">Doh! Please type in a gem name.</h3><br><p class="text-center"><img src="https://media.giphy.com/media/xT5LMESsx1kUe8Hiyk/giphy.gif"></p>');	
+    		} else {
+    			$('#searchResults').html('<h3 class="text-center">Oh snap! Couldn\'t find that gem. Please try again.</h3><br><p class="text-center"><img src="https://media.giphy.com/media/26AHLBZUC1n53ozi8/giphy.gif"></p>');
+    		}
+    		
+    	});
+  	}); // end click
+});
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
+//= app.js
 //= require_tree .
