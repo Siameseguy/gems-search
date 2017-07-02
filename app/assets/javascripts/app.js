b29faabb3f89ac727c00c1ccb77e1443e7be059f
Phanousit Syhakhom <phanousitsyhakhom@Phanousit.fios-router.home>
2017-07-02 11:50:01 -0400

    finished displaying dependencies and added new css

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -10,50 +10,50 @@ $(document).ready(function(){
   		const rubyGems = 'https://rubygems.org/api/v1/gems/' + searchVal + '.json';  
 
   		function getGems(data) {
-  			$.each(data, function(index,gem) {				
-  				let gemName = '<div class="panel panel-default">';
-  					gemName += '<div class="panel-heading"><h3 class="panel-title">Gem</h3></div>';
-  					gemName += '<div class="panel-body">' +
-  									'<a href="' + data.project_uri + '"' + 'target="_blank">' + data.name + '</a>' +
-  									'<span class="stars pull-right"><i class="fa fa-star-o" aria-hidden="true"></i></span>' +
-  								'</div>';
-  					gemName += '</div>';
+ 	$.each(data, function(index,gem) {	
+ 	let gemName = '<div class="panel panel-default">';
+ 	gemName += '<div class="panel-heading"><h3 class="panel-title">Gem</h3></div>';
+ 	gemName += '<div class="panel-body">' +
+ 	'<a href="' + data.project_uri + '"' + 'target="_blank">' + data.name + '</a>' +
+ 	'<span class="stars pull-right"><i class="fa fa-star-o" aria-hidden="true"></i></span>' +
+ 	'</div>';
+ 	gemName += '</div>';
 
-  					gemName += '<div class="panel panel-default">';
-  					gemName += '<div class="panel-heading"><h3 class="panel-title">Info</h3></div>';
-  					gemName += '<div class="panel-body">' +
-  									data.info +
-  								'</div>';
-  					gemName += '</div>';
+ 	gemName += '<div class="panel panel-default">';
+ 	gemName += '<div class="panel-heading"><h3 class="panel-title">Info</h3></div>';
+ 	gemName += '<div class="panel-body">' +
+ 	data.info +
+ 	'</div>';
+ 	gemName += '</div>';
 
-  					gemName += '<div class="panel panel-default">';
-  					gemName += '<div class="panel-heading"><h3 class="panel-title">Dependencies</h3></div>';
-  					gemName += '<div class="panel-body"><ul class="list-unstyled">' + 
-  									'<li><a href="https://rubygems.org/gems/' + 
-  											$.each(data.dependencies.runtime, function(i, dep){
-  												 dep.name
-  											}); + '" target="_blank">' + 
-  											data.dependencies.runtime[0].name + 
-  										'</a>' + 
-  										'<span class="stars pull-right">' + 
-  											'<i class="fa fa-star-o" aria-hidden="true"></i>' + 
-  										'</span>' + 
-  									'</li></ul>' + 
-  								'</div>';
-  					gemName += '</div>';
+ 	gemName += '<div class="panel panel-default">';
+ 	gemName += '<div class="panel-heading"><h3 class="panel-title">Dependencies</h3></div>';
+ 	gemName += '<div class="panel-body"><ul class="list-unstyled">';
+                   $.each(data.dependencies.runtime, function(i, dep){
+                        gemName += '<li><a href="https://rubygems.org/gems/' + dep.name + '" target="_blank">' + dep.name + '</a>' + 
+ 						'<span class="stars pull-right">' + 
+ 							'<i class="fa fa-star-o" aria-hidden="true"></i>' + 
+ 						'</span>' + 
+ 						'</li>';
+                   });
+                   gemName += '</ul>' + 
+ 	'</div>';
+ 	gemName += '</div>';
 
-  			$('#searchResults').html(gemName);
+  $('#searchResults').html(gemName);
 
 
-  			$('.stars').click(function(evt){
-  				const favoritesArray = [];
-  				favoritesArray.push(data.name);
-  				console.log(favoritesArray);
+ 	$('.stars').click(function(evt){
+ 	const favoritesArray = [];
+  favoritesArray.push(data.name);
+ 	console.log(favoritesArray);
 
-  				$('#favorites').html('hello world');
-  			});
-      }); // end each
-    	}
+ 	$('#favorites').html('hello world');
+ 	});
+     }); // end each
+   	}
+
+
     	$.getJSON(rubyGems, getGems).fail(function(){
     		if(searchVal === "") {
     			$('#searchResults').html('<h3 class="text-center">Doh! Please type in a gem name.</h3><br><p class="text-center"><img src="https://media.giphy.com/media/xT5LMESsx1kUe8Hiyk/giphy.gif"></p>');	
